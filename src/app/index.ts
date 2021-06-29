335e1797035db4929c439338b5d769fb30706def
Francis Lagares <francis.lagares@gmail.com>
2021-06-29 20:21:02 +0200

    feat: add cors middleware

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,13 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import indexRouter from '../routes';
+import planetsRouter from '../routes/planets/planets.router';
 
 const app = express();
 
 // Middlewares...
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes...
 app.use('/', indexRouter);
+app.use(planetsRouter);
 
 export { app };
