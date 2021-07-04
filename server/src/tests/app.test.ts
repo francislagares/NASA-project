3bfcead4dff1638aa10dcd1a8faad9dff80b647a
Francis Lagares <francis.lagares@gmail.com>
2021-07-04 17:41:27 +0200

    refactor: setup eslint, prettier at root folder

--- server/src/tests/app.test.ts
+++ server/src/tests/app.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../app';
 
 describe('App Index', () => {
-  test('[GET] / with response statusCode 200 ', async () => {
+  test('[GET] / with response statusCode 200', async () => {
     await request(app).get('/').expect(200);
   });
 });
