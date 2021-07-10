245acecbd4340dab3fa148a4164b2c1189505f07
Francis Lagares <francis.lagares@gmail.com>
2021-07-10 18:48:28 +0200

    feat: create launches mongoose model

---
+++ server/src/models/launches.mongo.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from 'mongoose';
+import { ILaunches } from '../types/launches';
+
+const launchesSchema: Schema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: String,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+export default mongoose.model<ILaunches>('Launches', launchesSchema);
