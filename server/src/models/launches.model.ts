2611ae8068b48962b2fb4631477527d86562e365
Francis Lagares <francis.lagares@gmail.com>
2021-07-15 18:34:01 +0200

    feat: load spacex data in api and map it to db

--- server/src/models/launches.model.ts
+++ server/src/models/launches.model.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable max-lines-per-function */
+import axios from 'axios';
 import launchesDB from './launches.mongo';
 import planets from './planets.mongo';
 import { ILaunches } from '../types/launches';
@@ -7,19 +10,66 @@ const DEFAULT_FLIGHT_NUMBER = 100;
 // const launches = new Map();
 
 const launch: ILaunches = {
-  flightNumber: 100,
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1',
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customers: ['ZTM', 'NASA'],
-  upcoming: true,
-  success: true,
+  flightNumber: 100, // flight_number
+  mission: 'Kepler Exploration X', // name
+  rocket: 'Explorer IS1', // rocket.name
+  launchDate: new Date('December 27, 2030'), // date_local
+  target: 'Kepler-442 b', // not applicable
+  customers: ['ZTM', 'NASA'], // payload.customer
+  upcoming: true, // upcoming
+  success: true, // success
 };
 
 saveLaunch(launch);
 
 // launches.set(launch.flightNumber, launch);
+
+const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
+
+async function loadLaunchData() {
+  console.log('Downloading launch data...');
+  const response = await axios.post(SPACEX_API_URL, {
+    query: {},
+    options: {
+      populate: [
+        {
+          path: 'rocket',
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: 'payloads',
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+  });
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc['payloads'];
+    // @ts-ignore
+    const customers = payloads.flatMap(payload => {
+      return payload['customers'];
+    });
+
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers,
+    };
+
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
+}
 
 async function existsLaunchWithId(launchId: number) {
   return await launchesDB.findOne({
@@ -88,6 +138,7 @@ async function abortLaunchById(launchId: number) {
 }
 
 export {
+  loadLaunchData,
   abortLaunchById,
   getAllLaunches,
   scheduleNewLaunch,
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import { app } from './app';
 import { loadPlanetsData } from './models/planets.model';
-import { loadLaunchesData } from './models/launches.model';
+import { loadLaunchData } from './models/launches.model';
 import { mongoConnect } from './services/mongodb';
 
 const createServer = async () => {
   try {
     await mongoConnect();
     await loadPlanetsData();
+    await loadLaunchData();
     // Start the server
     const PORT = 8000;
     // Binding Heroku to 0.0.0.0 instead of localhost
